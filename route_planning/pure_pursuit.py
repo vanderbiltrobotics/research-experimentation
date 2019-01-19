616df157ef42b02ee526a9e727a5730a3cf9ce13
Baibhav Vatsa <baibhavvatsa@gmail.com>
2019-01-19 16:53:09 -0600

    helper functions definitions for pure pursuit

--- route_planning/pure_pursuit.py
+++ route_planning/pure_pursuit.py
@@ -1,54 +1,66 @@
-//This is a sudo code for pure pursuit 
-//input: array of turning points
-//output: double linear_velocity, double angualr_velocity
+# This is the pseudo code for pure pursuit
+# Input: array of turning points
+# Output: double linear_velocity, double angular_velocity
 
-//helper functions
-  
-//a function that generate an array of vectors from the array of turning points
-genVectors(){
- 
-}
+from math import sqrt
+tp = [] #list of point coordinates
+n = len(tp)
 
-//a function that finds the point on the path with shortest distance to current location
-//p1,p2 are the endpoints of the current vector segment we are trying to identify a point on
-//cur is our current location
-genBase(p1,p2){
-  if(dotProduct(p1->p2,p1->cur)<0){
-    return p1
-  }
-  else if(dotProduct(p2->p1,p2->cur)<0){
-    //go to examine vector(p2,p3)
-    genbase(p2,p3)
-  }
-  else{
-  base.x=(cur.x-p1.x)*(p2.x-p1.x)/dis(p1,p2)
-  }
-}
+# Helper Functions
 
-//a function that dynamically updates the look_ahead_distance strlen
-strlen_update(){
-  strlen=20
-  return strlen
-}
+# dis: returns distance between two points
+# v1: the first point
+# v2: the second point. defaults to origin
+def dis(v1, v2 = (0,0)):
+    return sqrt((v1[0]-v2[0])^2 + (v1[1]-v2[1])^2)
 
+# dotprod: returns the dot product of two vectors
+# v1: the first vector
+# v2: the second vector. defaults to (1,0)
+def dotprod(v1, v2 = (1,0)):
+    return v1[0]*v2[0] + v1[1]*v2[1]
 
-//find the point that we are looking up to, look ahead point
-genTarget(strlen, curVector){
-  disRemain=dis(base,next closest turning point)
-  if(disRemain>=strlen){
-    return target_loc=base+curVector/curVectorLength*strlen
-  }
-  else {
-    genTarget(strlen-disRemain,curVector->next)
-  }
-}
+# projectbase: returns the point on the path with shortest distance to current location
+# cur: current position coordinates
+# i: the segment of the  path we are in
+def projectbase(cur, i):
+    if i < n:
+        segv = (tp[i+1][0]-tp[i][0], tp[i+1][1]-tp[i][1])
+        seglen = dis(tp[i], tp[i+1])
+        curlen = dotprod(cur, segv)
+        if curlen <= seglen:
+            return (curlen/seglen) * segv
+        else:
+            projectbase(cur, i+1)
+    return None
 
-//direct distance between two given points
-dis(p1,p2){
-  return((p1.x-p2.x)^2+(p1.y-p2.y)^2)
-}
+# updatestrdist: returns the new length of the distance between look ahead and current location
+def updatestrdist():
+    strdist = 20
+    return strdist
 
-//main
-main{
-  
-}
+# gentarget: returns the coordinates of the target
+# cur: the current position coordinates
+# i: the segment of path we are in
+def gentarget(cur, i):
+    base = projectbase(cur, i)
+    disremain = dis(base, tp[i+1])
+    strdist = updatestrdist()
+    #TODO: how to check the target will be in the next segment or this one
+#   if disremain >= strdist:
+#       target = base + cur / cur.length() * strdist
+#       return target
+#   else:
+#       gentarget(strdist - disremain, cur->next)
+
+# linear_vel: returns linear velocity
+def linear_vel(*args):
+    pass
+
+# angular_vel: returns the angular velocity
+def angular_vel(*args):
+    pass
+
+#main function
+def pure_pursuit():
+    pass
